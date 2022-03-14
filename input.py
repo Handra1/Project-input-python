37a76ef6d4cf07d0f58aed19d444035e535a5918
Han <101443278+Handra1@users.noreply.github.com>
2022-03-14 12:35:00 +0700

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+input("Tell me your name:")
+name = input("Who is your name:")
+print("Your name:" + name)
+"""text editor tidak bisa menampilkan hasil output
+ karena harus menggunakan terminal atau cmd untuk melihat hasilnya"""
+
+siblings_text = input("number of siblings you have:")
+siblings = int(siblings_text)
+print(siblings)
+"""jika tidak yakin untuk angka apakah integer atau float maka gunakan float"""
